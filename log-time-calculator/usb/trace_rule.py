a8f446d5adb4041b98ed6c0acab68174fc1fc5db
Pan Junzhong <panjunzhong@outlook.com>
2024-06-27 11:18:14 +0800

    1/2: use submodule for log-time-calculator

--- log-time-calculator/usb/trace_rule.py
+++
@@ -1,9 +0,0 @@
-rule=[
-    ("resume device", "resume-device"),
-    ("non generic driver resume", "resume-call"),
-    ("generic driver resume", "generic"),
-    ("port resume", "port"),
-    ("finish port resume", "finish-port"),
-]
-input_log = "1.log"
-output_csv = "result1.csv"
